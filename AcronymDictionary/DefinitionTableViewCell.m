8a7960d895d2358e68600a6267932630ffeb9abb
Isiah Manns <isiah.michael1018@gmail.com>
2018-06-15 12:22:24 -0700

    Modified TableView UI age label * The entire age label string appears in bold font * The [number of years] portion appears in bold and blue font

--- AcronymDictionary/DefinitionTableViewCell.m
+++ AcronymDictionary/DefinitionTableViewCell.m
@@ -18,7 +18,43 @@
 - (void)setDefinition:(AcronymDefinition*)definition
 {
     self.definitionLabel.text = definition.lf;
-    self.ageLabel.text = [NSString stringWithFormat:@"Used for %ld years", [definition age]];
+    
+    NSMutableAttributedString *ageLabelWithAttrs = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"Used for %ld years", [definition age]]];
+    
+    // Regex to find first occurence of an integer.
+    NSError *error = NULL;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"((\\d+) years)"
+                                                                           options:0
+                                                                             error:&error];
+    
+    NSArray<NSTextCheckingResult *> *matches = [regex matchesInString:ageLabelWithAttrs.string
+                                                              options:0
+                                                                range:NSMakeRange(0, ageLabelWithAttrs.string.length)];
+    
+    NSRange yearsQuantityRange;
+    NSRange yearsStrRange;
+    for (NSTextCheckingResult *match in matches) { // 3 total ranges [0-2], capture groups start at index 1
+        NSLog(@"%@", [ageLabelWithAttrs.string substringWithRange:[match rangeAtIndex:1]]); // $1
+        NSLog(@"%@", [ageLabelWithAttrs.string substringWithRange:[match rangeAtIndex:2]]); // $2
+        NSLog(@"%lu", (unsigned long)match.numberOfRanges);
+        
+        yearsQuantityRange = [match rangeAtIndex:2];
+        yearsStrRange = [match rangeAtIndex:1];
+    }
+
+    
+    NSDictionary *yearsQuantityAttrs = @{
+                                 NSFontAttributeName: [UIFont boldSystemFontOfSize: [UIFont systemFontSize]],
+                                 NSForegroundColorAttributeName: [UIColor blueColor]
+                                 };
+    
+    
+    [ageLabelWithAttrs addAttributes:yearsQuantityAttrs range:yearsQuantityRange];
+    [ageLabelWithAttrs addAttribute:NSFontAttributeName
+                              value:[UIFont boldSystemFontOfSize:[UIFont systemFontSize]]
+                              range:yearsStrRange];
+    
+    self.ageLabel.attributedText = ageLabelWithAttrs;
 }
 
 @end
